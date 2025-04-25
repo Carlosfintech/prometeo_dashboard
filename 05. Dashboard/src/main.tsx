863fc4b177c79f2d2db1e5b790aef9cdd7c00675
Carlosfintech <carlos.fintech@gmail.com>
2025-04-24 19:02:31 -0600

    chore: add complete 05. Dashboard folder

--- 05. Dashboard/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { registerCharts } from './components/dashboard/ChartConfig'
-
-// Register Chart.js components
-registerCharts();
-
-createRoot(document.getElementById("root")!).render(<App />);
